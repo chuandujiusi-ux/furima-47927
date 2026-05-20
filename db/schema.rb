711e90f56153bc300049b8d88f62f4477cfd4c54
chuandujiusi-ux <chuandujiusi@gmail.com>
2026-05-21 03:07:45 +0900

    新規登録・ログイン・ログアウト画面実装

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,15 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.1].define(version: 2026_05_15_071526) do
-  create_table "users", charset: "utf8mb3", force: :cascade do |t|
+  create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "nickname", null: false
+    t.string "last_name", null: false
     t.string "first_name", null: false
+    t.string "last_name_kana", null: false
+    t.string "first_name_kana", null: false
+    t.date "birthday", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
