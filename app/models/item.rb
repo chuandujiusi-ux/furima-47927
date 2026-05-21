4fc3e3c940880ab9b68b169d59e758216dec0b0e
chuandujiusi-ux <chuandujiusi@gmail.com>
2026-05-22 03:06:00 +0900

    エラーメッセージの日本語化設定(追加)

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,5 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
-
   belongs_to :category
   belongs_to :item_condition
   belongs_to :postage_payer
@@ -10,16 +9,20 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
-  validates :image,             presence: true
-  validates :name,              presence: true
-  validates :introduction,      presence: true
-  validates :price,             presence: true
+  validates :image,              presence: true
+  validates :name,               presence: true
+  validates :introduction,       presence: true
+  validates :price,              presence: true,
+                                 numericality: {
+                                   only_integer: true,
+                                   greater_than_or_equal_to: 300,
+                                   less_than_or_equal_to: 9_999_999,
+                                   allow_blank: true
+                                 }
 
-  validates :category_id, numericality: { other_than: 1, message: "can't be blank" }
-  validates :item_condition_id, numericality: { other_than: 1, message: "can't be blank" }
-  validates :postage_payer_id,   numericality: { other_than: 1, message: "can't be blank" }
-  validates :prefecture_id,      numericality: { other_than: 1, message: "can't be blank" }
-  validates :preparation_day_id, numericality: { other_than: 1, message: "can't be blank" }
-
-  validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+  validates :category_id,        numericality: { other_than: 1 }
+  validates :item_condition_id,  numericality: { other_than: 1 }
+  validates :postage_payer_id,   numericality: { other_than: 1 }
+  validates :prefecture_id,      numericality: { other_than: 1 }
+  validates :preparation_day_id, numericality: { other_than: 1 }
 end
