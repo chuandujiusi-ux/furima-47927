a53f84ea9806850ed4ec0e985661e1f0a46f3f30
chuandujiusi-ux <chuandujiusi@gmail.com>
2026-05-27 06:10:36 +0900

    会員情報入力エラーメッセージ修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,11 +6,11 @@ class User < ApplicationRecord
 
   validates :nickname, presence: true
 
-  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
-  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
+  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }, allow_blank: true
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }, allow_blank: true
 
-  validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー]+\z/ }
-  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー]+\z/ }
+  validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー]+\z/ }, allow_blank: true
+  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー]+\z/ }, allow_blank: true
 
   validates :birthday, presence: true
 
