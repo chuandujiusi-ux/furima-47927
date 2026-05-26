181a2e15c9e5ebbd0deaca3c5233214332234663
chuandujiusi-ux <chuandujiusi@gmail.com>
2026-05-27 04:21:25 +0900

    バリデーションメッセージの日本語化

--- app/models/item.rb
+++ app/models/item.rb
@@ -19,7 +19,7 @@ class Item < ApplicationRecord
     validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
   end
 
-  with_options numericality: { other_than: 1, message: "can't be blank" } do
+  with_options numericality: { other_than: 1, message: "を入力してください" } do
     validates :category_id
     validates :item_condition_id
     validates :postage_payer_id
